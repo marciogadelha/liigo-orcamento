7306908fb13e95befaf9900889aa350147f2385a
Márcio Gadelha <marciogadelha89@gmail.com>
2020-08-18 14:29:49 -0300

    Front-end

--- src/api.js
+++ src/api.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors')
 const Categories = require('./orcamento/Categories')
+const path = require('path')
 
 const api = express();
 
@@ -16,6 +17,18 @@ const categories = new Categories()
 api.get('/categories', cors(corsOptions), async (req, res) => {
     const response = await categories.getLayout()
     res.send(response)
+});
+
+api.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/index.html'))
+});
+
+api.get('/bootstrap.css', (req, res) => {
+    res.sendFile(path.join(__dirname + '/bootstrap.css'))
+});
+
+api.get('/style.css', (req, res) => {
+    res.sendFile(path.join(__dirname + '/style.css'))
 });
 
 module.exports = api;
