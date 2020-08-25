dc478aa7dcf585b167b46e11baa7265257722c08
Márcio Gadelha <marciogadelha89@gmail.com>
2020-08-24 21:20:52 -0300

    Redeploy.

--- src/orcamento/FlexyAPI.js
+++ src/orcamento/FlexyAPI.js
@@ -12,7 +12,7 @@ const FlexyAPI = class FlexyAPI {
     let response = null
     while(!response) {
       try {
-        console.log(info)
+        console.log("GET: " + info)
         response = await this.api.get(info, config)
         // console.log(response)
       } catch(error) {
