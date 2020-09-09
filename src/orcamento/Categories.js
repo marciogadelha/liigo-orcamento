28d892ff512d1028328ebde9cdd278815ee62dbc
Márcio Gadelha <marciogadelha89@gmail.com>
2020-09-09 19:27:27 -0300

    Wait for 2 days to make requests again.

--- src/orcamento/Categories.js
+++ src/orcamento/Categories.js
@@ -159,7 +159,7 @@ const Categories = class Categories {
   main = async () => {
     while (true) {
       await this.load()
-      await this.apiFlexy.sleep(86400000)
+      await this.apiFlexy.sleep(172800000)
     }
   }
 
