95f5cc7ad78a093195c824852100ff43bedb13b0
Márcio Gadelha <marciogadelha89@gmail.com>
2020-09-09 19:02:07 -0300

    Wait after run again.

--- src/orcamento/Categories.js
+++ src/orcamento/Categories.js
@@ -158,8 +158,8 @@ const Categories = class Categories {
 
   main = async () => {
     while (true) {
-      await this.apiFlexy.sleep(86400000)
       await this.load()
+      await this.apiFlexy.sleep(86400000)
     }
   }
 
